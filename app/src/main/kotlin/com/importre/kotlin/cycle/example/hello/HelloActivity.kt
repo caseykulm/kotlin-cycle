f3294b08cb0b85f4283078f786638ed315d604dc
Casey Kulm <casey.kulm@bazaarvoice.com>
2017-08-25 15:07:50 -0500

    Add Model-View-Intent pattern to Hello example

--- app/src/main/kotlin/com/importre/kotlin/cycle/example/hello/HelloActivity.kt
+++ app/src/main/kotlin/com/importre/kotlin/cycle/example/hello/HelloActivity.kt
@@ -5,6 +5,7 @@ import com.importre.kotlin.cycle.*
 import com.importre.kotlin.cycle.example.BaseActivity
 import com.importre.kotlin.cycle.example.R
 import kotlinx.android.synthetic.main.activity_hello.*
+import rx.Observable
 
 class HelloActivity : BaseActivity() {
 
@@ -15,10 +16,37 @@ class HelloActivity : BaseActivity() {
         Cycle.run(main, DomSource())
     }
 
+    /**
+     * Purpose: interpret DOM events as user’s intended actions
+     * Input: DOM source
+     * Output: Action Streams
+     */
+    private val intent = { domSource: DomSource ->
+        domSource.select(helloEdit).textChanges()
+    }
+
+    /**
+     * Purpose: manage state
+     * Input: Action Streams
+     * Output: State Stream
+     */
+    private val model = { actionStream: Observable<CharSequence> ->
+        actionStream.map(::greeting)
+    }
+
+    /**
+     * Purpose: visually represent state from the Model
+     * Input: State Stream
+     * Output: Stream of Virtual DOM nodes as the DOM Driver sink
+     */
+    private val view = { stateStream: Observable<String> ->
+        stateStream.map { message -> { helloText.text = message } }
+    }
+
     private val main = { sources: Sources ->
-        val change = sources.dom().select(helloEdit).textChanges()
-        val model = change.map(::greeting)
-        val view = model.map { message -> { helloText.text = message } }
+        val intent = intent(sources.dom())
+        val model = model(intent)
+        val view = view(model)
         Sinks(DomSink(view))
     }
 }
